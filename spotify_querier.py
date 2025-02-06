88a8791ab65f2a2f494805869dbc6c3849532d8c
matthieuDev <matthieu.devaux.epfl@outlook.com>
2025-02-07 00:47:30 +0100

    get_seed_to_playlist_from_track

--- spotify_querier.py
+++ spotify_querier.py
@@ -34,10 +34,34 @@ class spotify_querier :
         return response
     
     def get_playlist(self, playlist_id):
+        if playlist_id.startswith('spotify:playlist:'):
+            playlist_id = playlist_id.replace('spotify:playlist:', '')
         return self.get(
             f'https://api.spotify.com/v1/playlists/{playlist_id}',
             headers = {
                 'Authorization': f'Bearer {self.api_auth_token}'
             },
             cached_filename = f'playlist_{playlist_id}',
+        )
+    
+    def get_track(self, track_id):
+        if track_id.startswith('spotify:track:'):
+            track_id = track_id.replace('spotify:track:', '')
+        return self.get(
+            f'https://api.spotify.com/v1/tracks/{track_id}',
+            headers = {
+                'Authorization': f'Bearer {self.api_auth_token}'
+            },
+            cached_filename = f'track_{track_id}',
+        )
+    
+    def get_seed_to_playlist_from_track(self, track_id):
+        if track_id.startswith('spotify:track:'):
+            track_id = track_id.replace('spotify:track:', '')
+        return self.get(
+            f"https://spclient.wg.spotify.com/inspiredby-mix/v2/seed_to_playlist/spotify:track:{track_id}?response-format=json",
+            headers = {
+                'Authorization': f'Bearer {self.web_authorisation_token}'
+            },
+            cached_filename = f'seed_to_playlist_{track_id}',
         )
